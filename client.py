f70c127e7f1c1c180f9275cb63236cee19f3c4d5
Matt Sherrill <matthew@sherrillfamily.net>
2023-11-30 21:03:04 -0600

    Fixed persistent conenction issues.

--- client.py
+++ client.py
@@ -5,60 +5,39 @@ import sys
 # Setup socket information
 client_socket = socket.socket()
 port = 12234
-client_socket.connect(('127.0.0.1', port))
+client_socket.connect(('localhost', port))
 
 def printMenu():
-    choice = 404
-    # while choice != 4:
-    #     print('''
-    #     -----------------------------------------------------------------------------
-    #     --------------------     Welcome to SherrillATM!       ----------------------
-    #     -----------------------------------------------------------------------------\n
-    #     Please choose from one of the following options:
-    #     1: Deposit
-    #     2: Withdrawal
-    #     3: Check Balance
-    #     4: Exit SherrillATM
-    #     ''')
-    #     choice = input('Enter your choice: ')
-    #
-    #     if choice == '1':
-    #         deposit()
-    #     elif choice == '2':
-    #         withdrawal()
-    #     elif choice == '3':
-    #         tempBal = checkBalance()
-    #         print(f'Your balance is ${tempBal}.')
-    #         # checkBalance()
-    #     else:
-    #         choice = input('You did not choose one of the options. Try again: ')
-    #         continue
-    print('''
-    #     ----------------------------------------------------------------------------- 
-    #     --------------------     Welcome to SherrillATM!       ----------------------
-    #     -----------------------------------------------------------------------------\n
-    #     Please choose from one of the following options:
-    #     1: Deposit
-    #     2: Withdrawal
-    #     3: Check Balance
-    #     4: Exit SherrillATM
-    #     ''')
-    choice = input('Enter your choice: ')
+    choice = "null"
+    while choice != '4':
+        print('''
+        -----------------------------------------------------------------------------
+        --------------------     Welcome to SherrillATM!       ----------------------
+        -----------------------------------------------------------------------------\n
+        Please choose from one of the following options:
+        1: Deposit
+        2: Withdrawal
+        3: Check Balance
+        4: Exit SherrillATM
+        ''')
+        choice = input('Enter your choice: ')
 
-    if choice == '1':
-        deposit()
-    elif choice == '2':
-        withdrawal()
-    elif choice == '3':
-        tempBal = checkBalance()
-        print(f'Your balance is ${tempBal}.')
-        # checkBalance()
-    else:
-        choice = input('You did not choose one of the options. Try again: ')
-        # continue
-    client_socket.sendall('e'.encode())
-    client_socket.close()  # Close connection to server
-    exit()
+        if choice == '1':
+            deposit()
+        elif choice == '2':
+            withdrawal()
+        elif choice == '3':
+            tempBal = checkBalance()
+            print(f'Your balance is ${tempBal}.')
+            # checkBalance()
+        elif choice == '4':
+            client_socket.sendall('e'.encode())
+            print("Closing SherrillATM...")
+            client_socket.close()  # Close connection to server
+            exit()
+        else:
+            choice = input('You did not choose one of the options. Try again: ')
+            continue
 
 def deposit():
     # print('In deposit.')
@@ -78,20 +57,19 @@ def deposit():
             break
 
     dep_send = 'd' + str(depAmount)
-    print(f'Sending {dep_send}')
+    # print(f'Sending {dep_send}')
     client_socket.send(dep_send.encode())  # Sending deposit amount to the server, to add to the balance.
 
     response = client_socket.recv(1024).decode()
-    print(f'Response from server: {response}')
+    # print(f'Response from server: {response}')
     if 'y' in response:
         print(f'${depAmount} was successfully deposited into your account.')
     else:
         print("Deposit failed to reach server.")
 
 def withdrawal():
-    print("in withdrawal")
     # Check to see balance for error checking
-    balance = checkBalance()
+    serverBal = checkBalance()
     while True:
         drawAmount_str = input('Please enter the amount for withdrawal: ')
 
@@ -104,7 +82,7 @@ def withdrawal():
         if drawAmount < 0:
             print('Withdrawal amount cannot be negative.')
             continue
-        elif drawAmount > balance:
+        elif drawAmount > serverBal:
             print('Insufficient funds. Enter a smaller amount.')
             continue
         else:
@@ -123,7 +101,7 @@ def withdrawal():
 def checkBalance():
     client_socket.send('b'.encode())
     balance = client_socket.recv(1024).decode() # s
-    if balance[0] == 'r':
+    if 'r' in balance:
         balance = balance.strip('bdwrc')
         # print(f'Your balance is {balance}.')
     return int(balance)
--- server.py
+++ server.py
@@ -8,11 +8,11 @@ TIMEOUT_VAL = 20.0
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 12234
-server_socket.bind(('127.0.0.1', port))
+server_socket.bind(('localhost', port))
 server_socket.listen(1)
 userBalance = 100
 
-client_socket, client_address = server_socket.accept()
+
 print(f'Server started up. Starting up balance at ${userBalance}.')
 # print(f'Connection created from {client_address}. Balance: ${userBalance}')
 
@@ -25,46 +25,49 @@ while True:
         print(f'No incoming connections within the last {TIMEOUT_VAL} seconds. Exiting server.')
         break
 
-    try:
-        # Receive and send data
-        data = client_socket.recv(1024).decode()            # RECEIVE
-        print(f'Received {data}')
+    client_socket, client_address = server_socket.accept()
+    print(f"User connected from {client_address}")
+    while True:
+        try:
+            # Receive and send data
+            data = client_socket.recv(1024).decode()  # RECEIVE
+            # print(f'Received {data}')
 
-        if not data:
-            break
+            if not data:
+                break
 
-        if 'b' in data:
-            print('Received Balance Check.')
-            sendBalance = 'r' + str(userBalance)
-            client_socket.send(sendBalance.encode())        # SEND
-        elif 'd' in data:
-            print('Received Deposit')
-            add_amt = data.strip('bdrwc')
-            userBalance += int(add_amt)
-            print(f'Balance: ${userBalance}')
+            if 'b' in data:
+                print('Received Balance Check.')
+                sendBalance = 'r' + str(userBalance)
+                client_socket.send(sendBalance.encode())        # SEND
+            elif 'd' in data:
+                print('Received Deposit')
+                add_amt = data.strip('bdrwc')
+                userBalance += int(add_amt)
+                print(f'Added {add_amt}. New balance: ${userBalance}')
 
-            response = 'y' + str(userBalance)
-            client_socket.send(response.encode())           # SEND
-            print(f'Sending {response}.')
-        elif 'w' in data:
-            print('Received valid withdrawal.')
-            sub_amt = data.strip('bdwrc')
-            userBalance -= int(sub_amt)
-            print(f'Balance: ${userBalance}')
+                response = 'y' + str(userBalance)
+                client_socket.send(response.encode())           # SEND
+                # print(f'Sending {response}.')
+            elif 'w' in data:
+                print('Received valid withdrawal.')
+                sub_amt = data.strip('bdwrc')
+                userBalance -= int(sub_amt)
+                print(f'Subtracted {sub_amt}. New balance: ${userBalance}')
 
-            response = 'y' + str(userBalance)
-            client_socket.send(response.encode())
-            print(f'Sending {response}.')
-        elif 'e' in data:
-            print('Received ending signal.')
-            client_socket.close()
-            server_socket.listen(1)
-            client_socket, client_address = server_socket.accept()
-            # break
-    except Exception as e:
-        print('Server crashed :( ', e)
-        # client_socket.close()
-        exit(1)
+                response = 'y' + str(userBalance)
+                client_socket.send(response.encode())
+                # print(f'Sending {response}.')
+            elif 'e' in data:
+                print('Received ending signal.')
+                client_socket.close()
+                server_socket.listen(1)
+                client_socket, client_address = server_socket.accept()
+                # break
+        except Exception as e:
+            print('Server crashed :( ', e)
+            # client_socket.close()
+            exit(1)
 
 
 server_socket.close()
